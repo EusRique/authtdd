771a3ee17c5d26a02c7c626640dde2ae492da20f
Henrique <hiquesc_@hotmail.com>
2020-05-16 18:34:36 -0300

    feat: created file app, service and route

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+class AppController {
+  constructor() {
+    this.express = express();
+
+    this.middleawares();
+    this.routes();
+  }
+
+  middleawares() {
+    this.express.use(express.json());
+  }
+
+  routes() {
+    this.express.use(require("./routes"));
+  }
+}
+
+module.exports = new AppController().express;

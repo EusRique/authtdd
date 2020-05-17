54a84759b32b1f9aa914773db0a42293f6e135e8
Henrique <hiquesc_@hotmail.com>
2020-05-17 11:56:21 -0300

    test: configuration jest

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,9 @@
+describe('Authentication', () => {
+  it('Should receive JWT token when authenticated with valid credentials', () => {
+    const x = 2;
+    const y = 4;
+    const sum = x + y;
+
+    expect(sum).toBe(6);
+  });
+})
